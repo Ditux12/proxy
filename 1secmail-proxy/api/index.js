0b34533299a8d7b0751e652d1152e8a66efd6b1f
Ditux12 <diogolourenco2811@gmail.com>
2025-06-05 18:32:47 +0100

    Update index.js

--- 1secmail-proxy/api/index.js
+++ 1secmail-proxy/api/index.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+  // Habilitar CORS para qualquer origem
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Responder a requisições OPTIONS para CORS preflight
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const { action } = req.query;
 
   if (action === 'generate') {
